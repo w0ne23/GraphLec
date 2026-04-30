6cd261d22850cb6b33fe24982bb378a150ded565
rwajm <allium0324@gmail.com>
2026-04-30 21:06:08 +0900

    chore: 레거시 web 디렉터리 및 관련 코드 제거

--- web/lectures/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Lecture
-
-
-@admin.register(Lecture)
-class LectureAdmin(admin.ModelAdmin):
-    list_display = ("id", "stem", "title", "created_at")
-    search_fields = ("stem", "title")
